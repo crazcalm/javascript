55dfaa5e9d07e8625295dda3a0259c56c7ee8ff2
Marcus Willock <crazcalm@gmail.com>
2025-05-25 18:49:08 -0400

    add jsdoc to project

--- modules/bag.js
+++ modules/bag.js
@@ -1,15 +1,30 @@
 class Bag {
   length = 0;
 
+  /**
+   * Represents a Bag
+   * @constructor
+   */
   constructor() {
     this._array = new Array();
   }
 
+  /**
+   * Adds and item to the Bag
+   * @method
+   * @param {Any} item An item to be placed in the bag
+   * @returns {Bag} Returns a reference to this Bag instance
+   */
   add(item) {
     this._array.push(item);
     this.length++;
     return this;
   }
+  /**
+   * Removes and item from the Bag
+   * @param {Any} item
+   * @returns {Bag} Returns a reference to this Bag instance
+   */
   remove(item) {
     let result = undefined;
     let index = this._array.indexOf(item);
